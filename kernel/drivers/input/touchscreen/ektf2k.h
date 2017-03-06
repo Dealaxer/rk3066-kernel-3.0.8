22fc02a5a2e276c57049ff7cdc3a6417b4170bc5
Dealaxer <dealaxer@gmail.com>
2017-03-06 02:52:36 -0800

    Added Kernel for Cube U30GT-H

---
+++ kernel/drivers/input/touchscreen/ektf2k.h
@@ -0,0 +1,26 @@
+/* include/linux/i2c/elan_ektf2k.h - ELAN EKTF2136 touchscreen driver
+ *
+ * Copyright (C) 2011 Elan Microelectronics Corporation.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#define ELAN_X_MAX 704	
+
+#define ELAN_Y_MAX 1088
+
+#ifndef _LINUX_ELAN_KTF2K_H
+#define _LINUX_ELAN_KTF2K_H
+#define ELAN_KTF2K_NAME "ekt2xxx"
+#define MAX_FINGER_NUM 5
+
+#endif /* _LINUX_ELAN_KTF2K_H */
+
+
